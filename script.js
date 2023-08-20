f5bc269b00584aeab45b789883f4fa5409e5f879
Darya Repchenko13 <daryarepchenko88@gmail.com>
2023-08-20 23:02:40 +0600

    check homework_2

---
+++ script.js
@@ -0,0 +1,69 @@
+// Задание 1 
+
+let a = 10;
+alert(a);
+
+// Задание 2
+
+let firstYear = ('29 июня 2007 год');
+alert (firstYear);
+
+// Задание 3
+
+let nameUser = ('Brendan Eich');
+alert (nameUser);
+
+// Задание 4
+
+let c = 10;
+let b = 2;
+alert(c + b);
+alert(c - b);
+alert(c * b);
+alert(c / b);
+
+// Задание 5
+
+const result = 2**5;
+console.log(result);
+
+// Задание 6
+
+let d = 9;
+let e = 2;
+alert(d % e);
+
+// Задание 7
+
+let numNew = 1;
+numNew += 5;
+numNew -= 3;
+numNew *= 7;
+numNew /= 3;
+numNew++;
+numNew--;
+alert(numNew);
+
+
+// Задание 8
+
+let age = prompt ('Сколько Вам лет?');
+alert(age);
+
+// Задание 9
+
+const user = {
+    nameFirst: ('Vitaliy'),
+    age: '35',
+    isAdmin: 'VR',
+} 
+user ['city of residence']= город
+user.age = 30
+delete user ['city of residence']
+let info = ('Какую информацию хотите узнать о пользователе?')
+alert(user[info])
+
+// Задание 10
+
+let yuorName = prompt('Как Вас зовут?')
+alert('Привет, ${yuorName}!')
