28072fc63efc98f0a0017585030f8b5145a287d4
Darya Repchenko13 <daryarepchenko88@gmail.com>
2023-10-26 14:55:10 +0600

    Проверить исправления по дз 2.5

---
+++ hw_5_2.js
@@ -0,0 +1,25 @@
+
+function whatSeason() {
+  let month = Number(prompt(`Введите номер месяца`));
+
+  if (month === 1 || month === 2 || month === 12) {
+    return alert(`Зима`);
+    
+  } else if (month === 3 || month === 4 || month === 5) {
+    return alert(`Весна`);
+
+  } else if (month === 6 || month === 7 || month === 8) {
+    return alert(`Лето`);
+
+  } else if (month === 9 || month === 10 || month === 11) {
+    return alert(`Осень`);
+
+  }  else {
+    return alert(`Вы ввели неверное значение`);
+
+  }
+
+}
+
+whatSeason();
+
